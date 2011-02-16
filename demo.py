51d656b03dc20b13fd74fb98e6ab293f57f709ff
jaymz campbell <jaymzcd@googlemail.com>
2011-02-16 01:17:18 +0000

    updated color list and demo

--- demo.py
+++ demo.py
@@ -7,7 +7,19 @@ def printstuff():
 @AnsiPrettyPrint('yellow')
 def morethings():
     print "i should be yellow"
+    x = range(1, 20)
+    AnsiColors.activate_color('blue', mode='bg')
+    for i in x:
+        print i
 
 printstuff()
 morethings()
+
+AnsiColors.activate_color('yellow', mode='bg')
+AnsiColors.activate_color('red', mode='fg')
+print "hey there"
+
+AnsiColors.reset()
+
+
 print "I come after"
--- pyprettyansi/prettyansi.py
+++ pyprettyansi/prettyansi.py
@@ -1,13 +1,27 @@
 class AnsiColors(object):
-    color_prefix = '\033[%dm'
+    """ Defines the standard term codes for various colors and a number of
+        methods to activate colors either via the class itself or an instance
+        of it which is slightly configurable """
+
+    color_prefix = '\033[%dm' # standard term code to open color sequence
+
+    # Following dict holds the color codes for fb/bg and effects
     colors = {
         'fg': {
             'blue': 34,
             'yellow': 33,
+            'red': 31,
+            'green': 32,
+            'cyan': 36,
+            'white': 37,
         },
         'bg': {
             'blue': 44,
             'yellow': 43,
+            'red': 41,
+            'green': 42,
+            'cyan': 46,
+            'white': 47,
         },
     }
 
@@ -15,13 +29,27 @@ class AnsiColors(object):
         self.mode = mode
 
     def set_color(self, color):
-        print self.color_prefix % self.colors[self.mode][color],
+        return self.activate_color(color, self.mode)
+
+    @staticmethod
+    def activate_color(color, mode='fg'):
+        """ You can use this method straight from the class rather than using
+            an existing AnsiColors instance """
+        print AnsiColors.color_prefix % (AnsiColors.colors[mode][color])
 
     @property
     def end(self):
-        print self.color_prefix % 0,
+        """ Terminate a given ansi color string sequence """
+        print AnsiColors.color_prefix % 0,
+
+    @staticmethod
+    def reset():
+        """ Helper method to terminate when colors set via activate_color """
+        print AnsiColors.color_prefix % 0,
 
 class AnsiPrettyPrint(object):
+    """ A decorator that sets the given ansi terminal color for a passed
+        functions output which is printed """
 
     def __init__(self, color):
         self.color = color
