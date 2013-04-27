0f2d4506a2afd39cc23fa77c5298b223a07167f0
jaymz campbell <jaymzcd@googlemail.com>
2013-04-27 17:48:02 +0200

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='prettyansi',
+    version='0.1',
+    author='Jaymz Campbell',
+    author_email='jaymz@jaymz.eu',
+    packages=['prettyansi', ],
+    url='http://github.com/jaymzcd/pyprettyansi',
+    license='LICENSE.txt',
+    description='Set colors for ansi terminal printing',
+)
